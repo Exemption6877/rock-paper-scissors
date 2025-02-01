bec336dd4d34ae6537988c09267561019329ede4
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-02 00:18:44 +0300

    Implement playRound()

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 let humanScore = 0;
 let computerScore = 0;
 
-
-function getComputerChoice(){
-    let random = Math.floor(Math.random()* 3);
-    if (random === 1) {
-        return "Rock";
-    } else if (random === 2) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3);
+  if (random === 1) {
+    return "rock";
+  } else if (random === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
-    return prompt("Enter your choice: ");
+  let choice = prompt("Enter your choice: ");
+  return choice.toLocaleUpperCase();
 }
 
 function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    return "draw";
+  }
 
+  if (
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
 }
