558743d606d08a30768f8a959e1f4f894b579411
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-01 23:56:26 +0300

    Make computerchoise return string choise

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ let computerScore = 0;
 
 
 function getComputerChoice(){
-    return Math.floor(Math.random()* 3);
+    let random = Math.floor(Math.random()* 3);
+    if (random === 1) {
+        return "Rock";
+    } else if (random === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
 
 function getHumanChoice() {
     return prompt("Enter your choice: ");
+}
+
+function playRound(humanChoice, computerChoice) {
+
 }
