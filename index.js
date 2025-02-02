f9d7a840723d86f9e5f905036090812e3ae8852a
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-02 06:19:16 +0300

    Redefine game logic to change DOM. Add ability to replay the game.

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 let humanScore = 0;
 let computerScore = 0;
 let buttonChoice = "";
+let gameActive = true;
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
+const output = document.querySelector(".output");
+const heading2 = document.createElement("h2");
+const scoreOutput = document.createElement("h1");
+
+heading2.textContent = "Press a button to play!";
+scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+
+output.appendChild(heading2);
+output.appendChild(scoreOutput);
+
 rockButton.addEventListener("click", (event) => {
-  buttonChoice = event.target.id;
-  playGame();
+  if (gameActive) {
+    buttonChoice = event.target.id;
+    gameActive = true;
+    playGame();
+  } else {
+    resetGame();
+  }
 });
 
 paperButton.addEventListener("click", (event) => {
-  buttonChoice = event.target.id;
-  playGame();
+  if (gameActive) {
+    buttonChoice = event.target.id;
+    gameActive = true;
+    playGame();
+  } else {
+    resetGame();
+  }
 });
 
 scissorsButton.addEventListener("click", (event) => {
-  buttonChoice = event.target.id;
-  playGame();
+  if (gameActive) {
+    buttonChoice = event.target.id;
+    gameActive = true;
+    playGame();
+  } else {
+    resetGame();
+  }
 });
 
 function getComputerChoice() {
@@ -43,10 +69,12 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == "scissors" && computerChoice == "paper")
   ) {
     humanScore++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+    heading2.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
   } else {
     computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+    heading2.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
   }
 }
 
@@ -54,22 +82,35 @@ function playGame() {
   const humanSelection = buttonChoice;
   const computerSelection = getComputerChoice();
 
+  heading2.textContent = `Press a button to play!`;
+
   // parse the choice to the DOM
   console.log(humanSelection);
   console.log(computerSelection);
 
   playRound(humanSelection, computerSelection);
 
-  if (humanScore === 5) {
-    console.log("Human wins!");
-    resetGame();
-  } else if (computerScore === 5) {
-    console.log("Computer wins!");
-    resetGame();
+  endGame();
+}
+
+function endGame() {
+  if (humanScore == 5) {
+    scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+    heading2.textContent = `Game Won! Press a button to play again.`;
+    gameActive = false;
+  } else if (computerScore == 5) {
+    scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+    heading2.textContent = `Game Lost! Press a button to play again.`;
+    gameActive = false;
   }
 }
 
 function resetGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+  if (!gameActive) {
+    humanScore = 0;
+    computerScore = 0;
+    heading2.textContent = "Press a button to play!";
+    scoreOutput.textContent = `You: ${humanScore} : Computer:${computerScore}`;
+    gameActive = true;
+  }
 }
